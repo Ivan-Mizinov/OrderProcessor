e1dcdedbf5c7288a386aba19e8b3737fcb4b90c4
Ivan Mizinov <mizinov.ivan@yandex.ru>
2025-10-08 12:33:12 +0300

    добавил класс Main для тестов

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class Main {
+    static void main() {
+        List<Order> testOrders = List.of(
+                new Order(1, "confirmed"),
+                new Order(2, "shipped"),
+                new Order(3, "pending"),
+                new Order(4, "confirmed"),
+                new Order(5, "shipped")
+        );
+
+        OrderProcessor processor = new OrderProcessor(testOrders);
+
+        System.out.println("Начинаем обработку заказов...\n");
+        processor.processOrders();
+        System.out.println("\nОбработка заказов завершена.");
+    }
+}
